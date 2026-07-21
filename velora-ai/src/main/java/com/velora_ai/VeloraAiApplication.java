16208519d5ac4f79c676b49ff67a811c55196856
Kalees-Nov <kaleeswaran0711@gmail.com>
2026-07-21 11:49:16 +0530

    initialize project

---
+++ velora-ai/src/main/java/com/velora_ai/VeloraAiApplication.java
@@ -0,0 +1,13 @@
+package com.velora_ai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VeloraAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VeloraAiApplication.class, args);
+	}
+
+}
---
+++ velora-ai/src/test/java/com/velora_ai/VeloraAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.velora_ai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VeloraAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
